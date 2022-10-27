add85685752f240e026f418b28f8515afffeaa44
Patryk Wiśniewski <patrickwisniewski91@gmail.com>
2022-10-27 16:23:46 +0200

    dodanie modelu FuelCar

---
+++ MVCDemo/Models/ElectricCar.cs
@@ -0,0 +1,25 @@
+﻿using MVCDemo.Interfaces;
+
+namespace MVCDemo.Models
+{
+    class ElectricCar : Car, IAndroidAudio
+    {
+        public ElectricCar() : base("Tesla", "p90") { }
+        public override Energy EnergyType => Energy.Electric;
+
+        public void BluetoothConnection()
+        {
+            Console.WriteLine("Bluetooth connected");
+        }
+
+        public override void DisplayStatus()
+        {
+            base.DisplayStatus();
+        }
+
+        public override void SpeedUp()
+        {
+            Speed += 4;
+        }
+    }
+}
