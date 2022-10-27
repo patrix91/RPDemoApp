5f9642f813987f9ad90d920ed9bf1993801d32ac
Patryk Wiśniewski <patrickwisniewski91@gmail.com>
2022-10-27 16:23:17 +0200

    dodanie modelu Car

---
+++ MVCDemo/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace MVCDemo.Models
+{
+    class Car : Vehicle
+    {
+        public Car(string brand, string type)
+        {
+            Brand = brand;
+            Type = type;
+        }
+
+        public string Brand { get; }
+        public string Type { get; }
+
+        public override Energy EnergyType => Energy.NotSpecified;
+
+        public override void DisplayStatus()
+        {
+            //base.DisplayStatus();
+            Console.WriteLine($"Speed {Speed} Brand {Brand} Type {Type} Energy {EnergyType}");
+        }
+    }
+}
