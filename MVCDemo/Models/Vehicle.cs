fddc99b210f3522a1975d32f841911828610cb72
Patryk Wiśniewski <patrickwisniewski91@gmail.com>
2022-10-27 16:22:35 +0200

    dodanie klasy abstrakcyjnej Vehicle

---
+++ MVCDemo/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace MVCDemo.Models
+{
+    abstract class Vehicle
+    {
+        public int Speed { get; protected set; }
+
+        virtual public void SpeedUp()
+        {
+            Speed++;
+        }
+
+        public virtual void DisplayStatus()
+        {
+            Console.WriteLine($"Speed {Speed} Energy {EnergyType}");
+        }
+
+        abstract public Energy EnergyType { get; }
+
+    }
+}
