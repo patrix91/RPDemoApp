6ce71af2f05e7a6df4ab0a1171ba613257513426
Patryk Wiśniewski <patrickwisniewski91@gmail.com>
2022-10-27 18:15:20 +0200

    zmiana nazwy pliku Interface.cs -> IRepository.cs dodanie własciwości Cars, oraz metod SaveChanges i Add

---
+++ MVCDemo/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using MVCDemo.Models;
+
+namespace MVCDemo.Interfaces
+{
+    public interface IRepository
+    {
+        public IQueryable<Car> Cars { get; }
+
+        void Add<EntityType>(EntityType entityType);
+
+        void SaveChanges();
+    }
+}
