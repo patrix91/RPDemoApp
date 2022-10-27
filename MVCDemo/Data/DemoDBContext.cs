1689d17b2d9e4e7fd540b1bf08269c20cc3a4081
Patryk Wiśniewski <patrickwisniewski91@gmail.com>
2022-10-27 16:46:21 +0200

    dodanie do klasy DemoDBContext obiektu DbSet<Car>

--- MVCDemo/Data/DemoDBContext.cs
+++ MVCDemo/Data/DemoDBContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MVCDemo.Models;
 
 namespace MVCDemo.Data
 {
@@ -8,5 +9,7 @@ namespace MVCDemo.Data
         {
             
         }
+
+        public DbSet<Car> Cars { get; set; }
     }
 }
